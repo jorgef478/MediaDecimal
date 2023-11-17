08a9d80b6351935389a436df7dfc9c6330ed4bd6
jorgef478 <jorgef478@gmail.com>
2023-11-17 20:47:53 -0300

    Arquivo finalizado

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+Console.Write("Digite a quantidade de numeros a serem exibidos.: ");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+
+decimal soma = 0;
+decimal numeroMenor = 999999999.99m;
+decimal numeroMaior = 999999999.99m;
+decimal digitado;
+int contador = 0;
+
+while (contador < quantidade)
+{
+    contador += 1;
+    Console.Write($"Número #{contador}: ");
+    digitado = Convert.ToDecimal(Console.ReadLine());
+    soma = soma + digitado;
+
+    if (contador == 1)
+    {
+        numeroMaior = digitado;
+        numeroMenor = digitado;
+    }   
+
+    if (digitado > numeroMaior)
+    {
+        numeroMaior = digitado;
+    }
+    if (digitado < numeroMenor)
+    {
+        numeroMenor = digitado;
+    }
+}
+
+if (contador > 0)
+    {
+        decimal media = soma / quantidade;
+        Console.WriteLine($"\nSoma...: {soma}");
+        Console.WriteLine($"Média..: {media:n2}");
+        Console.WriteLine($"Número maior..: {numeroMaior:n2}");
+        Console.WriteLine($"Número menor..: {numeroMenor:n2}");
+    }
